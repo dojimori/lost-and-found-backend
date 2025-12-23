ec5a8997fbd228850954129275a286fc32efaca6
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-24 00:56:47 +0800

    migrated to mysql

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,22 @@
 import "dotenv/config";
 import { PrismaClient } from '../../generated/prisma/client'
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5
+})
+
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+    await prisma.$connect();
+    console.log('mongodb connected')
+}
+
+main();
 
 export { prisma }
