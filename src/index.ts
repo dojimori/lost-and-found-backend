1fadc3257af504aae126eb5cf925c250c35f4cf5
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-23 08:36:20 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import type { Request, Response, Express } from "express";
+
+
+const app: Express = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`listening now on http://localhost:${PORT}`);
+});
