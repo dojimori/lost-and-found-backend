0311396ef111d7e2211a59a105dc091945bc9b4f
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-23 22:02:13 +0800

    register controller

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const app: Express = express();
 const PORT = 3000;
 
 app.use('/public', express.static('uploads'))
+app.use(express.json())
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,33 @@
 import type { Request, Response } from "express";
+import { prisma } from "../../lib/prisma";
+import bcrypt from 'bcryptjs'
 
-export const register = async(req: Request, res: Response) => {
+
+export const register = async (req: Request, res: Response) => {
     try {
-        // validate 
         const { name, email, password } = req.body;
 
-        
+        // check if the email already existed
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
 
-    } catch(error) {
+        if (user) {
+            return res.status(409).json({ message: 'User already exists.' });
+        }
+        const salt = bcrypt.genSaltSync(10);
+        await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: bcrypt.hashSync(password, salt)
+            }
+        })
+
+        res.status(201).json({ message: 'Account registered successfully.' })
+    } catch (error) {
         res.status(500).json(error)
         console.log(error)
     }
