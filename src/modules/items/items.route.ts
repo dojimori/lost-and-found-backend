bb89ace0f72ddafb17a691253b990fcb9058b260
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-25 16:43:59 +0800

    items module

---
+++ src/modules/items/items.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+// import { login, register } from './items.controller';
+
+const router = Router();
+router.get('/items',);
+router.post('/items',);
+
+export default router;
