ddedee8adf0f8ed30c02335ed1c4b5829f21a1e9
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-25 16:45:44 +0800

    items controller

--- src/index.ts
+++ src/index.ts
@@ -52,51 +52,6 @@ const upload = multer({ storage })
 app.use('/api/auth', authRoutes)
 
 
-app.get('/api/items', authMiddleware, async (req: Request, res: Response) => {
-    try {
-        // res.status(200).json({ tempDB });
-        const lostItems = await prisma.lostItem.findMany({
-            include: {
-                founder: true
-            }
-        });
-        res.status(200).json({ lostItems });
-
-    } catch (error) {
-        res.status(500).json({ message: 'something went wrong' })
-        console.log(error)
-    }
-})
-
-
-app.post('/api/items', authMiddleware, upload.single('image'), async (req: Request, res: Response) => {
-    try {
-        // TODO: assign a post to a user (which is the founder of the lost item)
-        const { itemName, description } = req.body;
-        const file = req.file;
-
-
-        if (!file) {
-            return res.status(409).json({ message: 'Image is required' });
-        }
-
-        await prisma.lostItem.create({
-            data: {
-                name: itemName,
-                description,
-                image: file.filename,
-                founder: {
-                    connect: { id: (req as any).user.id }
-                }
-            }
-        })
-
-        res.status(201).json({ message: 'lost item posted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'something went wrong' })
-        console.log(error)
-    }
-})
 
 
 
---
+++ src/modules/items/items.controller.ts
@@ -0,0 +1,48 @@
+import { prisma } from "../../lib/prisma";
+import type { Request, Response } from "express";
+
+const getItems = async (req: Request, res: Response) => {
+    try {
+        // res.status(200).json({ tempDB });
+        const lostItems = await prisma.lostItem.findMany({
+            include: {
+                founder: true
+            }
+        });
+        res.status(200).json({ lostItems });
+
+    } catch (error) {
+        res.status(500).json({ message: 'something went wrong' })
+        console.log(error)
+    }
+}
+
+
+const storeItem = async (req: Request, res: Response) => {
+    try {
+        // TODO: assign a post to a user (which is the founder of the lost item)
+        const { itemName, description } = req.body;
+        const file = req.file;
+
+
+        if (!file) {
+            return res.status(409).json({ message: 'Image is required' });
+        }
+
+        await prisma.lostItem.create({
+            data: {
+                name: itemName,
+                description,
+                image: file.filename,
+                founder: {
+                    connect: { id: (req as any).user.id }
+                }
+            }
+        })
+
+        res.status(201).json({ message: 'lost item posted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'something went wrong' })
+        console.log(error)
+    }
+}
