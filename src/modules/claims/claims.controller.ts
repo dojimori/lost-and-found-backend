382e32d93948e550984632ca07316ef64a5064fa
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-27 17:09:33 +0800

    can only claim an item once

--- src/modules/claims/claims.controller.ts
+++ src/modules/claims/claims.controller.ts
@@ -2,46 +2,59 @@ import { Response, Request } from "express";
 import { prisma } from "../../lib/prisma";
 import { connect } from "node:http2";
 
-
 export const claimItem = async (req: Request, res: Response) => {
   try {
-      // first is getting the item
-      const { postId, message, clue1, clue2, clue3 } = req.body
-      console.log(req.body)
-      const item = await prisma.lostItem.findUnique({
-        where: { id: postId },
-        include: {
-          founder: true
-        }
-      });
-      
-      if (!item) {
-        return;
-      }
+    // first is getting the item
+    const { postId, message, clue1, clue2, clue3 } = req.body;
+    console.log(req.body);
+    const item = await prisma.lostItem.findUnique({
+      where: { id: postId },
+      include: {
+        founder: true,
+      },
+    });
 
-      // second
-      const claim = await prisma.claim.create({
-        data: {
-          message,
-          clue1: clue1 ?? undefined,
-          clue2: clue2 ?? undefined,
-          clue3: clue3 ?? undefined,
-          founder: {
-            connect: {
-              id: item.founder.id
-            }
-          },
-          claimee: {
-            connect: { id: (req as any).user.id }
-          },
-          item: {
-            connect: { id: item.id }
-          }
-        }
-      })
+    if (!item) {
+      return;
+    }
 
-      return res.status(200).json(claim)
+    // check if the user already claimed the item
+    const findClaim = await prisma.claim.findFirst({
+      where: {
+        claimee: { id: (req as any).user.id },
+        founder: { id: item.founder.id },
+        item: { id: item.id },
+      },
+    });
+
+    if (findClaim) {
+      return res.status(409).json({ message: "You already claimed this item." });
+    }
+
+    // create
+    const claim = await prisma.claim.create({
+      data: {
+        message,
+        clue1: clue1 ?? undefined,
+        clue2: clue2 ?? undefined,
+        clue3: clue3 ?? undefined,
+        founder: {
+          connect: {
+            id: item.founder.id,
+          },
+        },
+        claimee: {
+          connect: { id: (req as any).user.id },
+        },
+        item: {
+          connect: { id: item.id },
+        },
+      },
+    });
+
+    return res.status(200).json(claim);
   } catch (error) {
-    console.log(error)
+    return res.status(500).json(error);
+    console.log(error);
   }
-}
+};
