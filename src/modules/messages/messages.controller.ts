a88edc59c4966a466d3baf5e69f58a12d57ae9ba
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-28 17:06:55 +0800

    message controller

---
+++ src/modules/messages/messages.controller.ts
@@ -0,0 +1,55 @@
+import { prisma } from '../../lib/prisma'
+import { Request, Response } from 'express'
+
+
+export const storeMessage = async(req: Request, res: Response) => {
+  try {
+    // senderid, receiverid, message
+    const { senderId, receiverId, message } = req.body;
+
+    await prisma.message.create({
+      data: {
+        message,
+        sender: {
+          connect: { id: senderId}
+        },
+        receiver: {
+          connect: { id: receiverId }
+        }
+      }
+    })
+
+    res.status(201).json({ message: "" })
+  } catch(error) {
+    res.status(500).json({ error })
+    console.log(error)
+  }
+}
+
+export const getMessages = async(req: Request, res: Response) => {
+  try {
+    // soooooo, we'll get the authed user's id
+    // and the chats associated with a specific user (receiver)
+
+    // const { senderId, receiverId, message } = req.body;
+    const receiverId = req.params.id;
+    const userId = (req as any).user.id;
+
+    const messages = await prisma.message.findMany({
+      where: {
+        sender: {
+          id: receiverId
+        },
+        receiver: {
+          id: userId
+        }
+      }
+    })
+
+
+    res.status(201).json({ messages })
+  } catch(error) {
+    res.status(500).json({ error })
+    console.log(error)
+  }
+}
